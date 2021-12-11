24647c9e43bf74deefea303b51ca294ff6ee65d2
Luke Wood <luke.wood@outlook.com>
2021-12-11 11:16:58 +1100

    removed compiled dir

--- compiled/helpers/formatConfigKey.js
+++
@@ -1,4 +0,0 @@
-export function formatConfigKey(configNameString) {
-    const frag = configNameString.split(' ');
-    return `${frag[0].toLowerCase()}${frag.slice(1).join('')}`;
-}
--- compiled/helpers/makeHandlers.js
+++
@@ -1,7 +0,0 @@
-import { formatConfigKey } from './formatConfigKey';
-export default function makeConfigHandlers({ config, handlerFactory, configName, configValue }) {
-    return config.reduce((handlers, config) => {
-        handlers[formatConfigKey(config[configName])] = handlerFactory(config[configName])(config[configValue]);
-        return handlers;
-    }, {});
-}
